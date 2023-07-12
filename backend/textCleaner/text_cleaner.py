926231c6eefce06ae6bdcce048492f5ca4d4e24f
akempins <01178535@pw.edu.pl>
2023-07-12 11:14:01 +0200

    Prototype of text cleaner

---
+++ backend/textCleaner/text_cleaner.py
@@ -0,0 +1,52 @@
+import spacy
+from spacy.matcher import Matcher
+import re
+import strenum
+
+
+PHONE_REGEX = r'(?:(?:(?:\+|00)?48)|(?:\(\+?48\)))?(?:1[2-8]|2[2-69]|3[2-49]|4[1-8]|5[0-9]|6[0-35-9]|[7-8][1-9]|9[145])\d{7}'
+ENTITY_TYPES = ['persName', 'geogName']
+REPLACEMENT = '[private]'
+
+
+class Engine(strenum.StrEnum):
+    PL = 'pl_core_news_lg'
+    EN = 'en_core_web_lg'
+
+
+def anonymize_text(text: str, engine: str) -> str:
+    nlp = spacy.load(engine)
+    doc = nlp(text)
+
+    email_pattern = [{"LIKE_EMAIL": True}]
+    url_pattern = [{"LIKE_URL": True}]
+
+    matcher = Matcher(nlp.vocab)
+    matcher.add("EMAIL", [email_pattern])
+    matcher.add("URL", [url_pattern])
+
+    matches = [str(match) for match in matcher(doc, as_spans=True)]
+    matches += re.findall(PHONE_REGEX, text)
+
+    anonymized_text = text
+
+    for ent in doc.ents:
+        if ent.label_ in ENTITY_TYPES:
+            anonymized_text = anonymized_text.replace(ent.text, REPLACEMENT)
+
+    for match in matches:
+        anonymized_text = anonymized_text.replace(match, REPLACEMENT)
+    
+    return anonymized_text
+
+
+if __name__ == '__main__':
+    text = ''
+    with open('test_txt_files/sample_cleaning_text.txt', 'r') as f:
+        text = f.read()
+
+    print('\nOriginal text:')
+    print(text)
+
+    print('\nAnonymized text:')
+    print(anonymize_text(text, ENGINES['pl']))

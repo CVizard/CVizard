0d17172bb643164125186c7deb5e27ac162ece20
akempins <01178535@pw.edu.pl>
2023-07-14 11:14:46 +0200

    added env loading in both pdf cleaner and reader

--- backend/textCleaner/main.py
+++ backend/textCleaner/main.py
@@ -1,20 +1,21 @@
 from kafka_connector import connect_kafka_producer, connect_kafka_consumer
 from text_cleaner import anonymize_text, Engine
+import os
 
 
-INPUT_TOPIC_NAME = 'pdf-text'
-OUTPUT_TOPIC_NAME = 'cleaned-text'
-BOOTSTRAP_SERVERS = ['kafka:29092']
+input_topic_name = os.environ.get('INPUT_TOPIC_NAME')
+output_topic_name = os.environ.get('OUTPUT_TOPIC_NAME')
+bootstrap_servers = [os.environ.get('BOOTSTRAP_SERVERS')]
 
 
 def main():
-    producer = connect_kafka_producer(BOOTSTRAP_SERVERS)
-    consumer = connect_kafka_consumer(BOOTSTRAP_SERVERS, INPUT_TOPIC_NAME)
+    producer = connect_kafka_producer(bootstrap_servers)
+    consumer = connect_kafka_consumer(bootstrap_servers, input_topic_name)
 
     for msg in consumer:
         text = msg.value.decode('utf-8')
         anonymized_text = anonymize_text(text, Engine.PL)
-        producer.send(OUTPUT_TOPIC_NAME, anonymized_text.encode('utf-8'))
+        producer.send(output_topic_name, anonymized_text.encode('utf-8'))
 
 
 if __name__ == '__main__':

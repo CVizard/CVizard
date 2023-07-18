00ca50d62fccc6dda11170a74a86f6467f990d6e
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-18 13:21:55 +0200

    add AI to pdf converter

--- backend/converter/src/main/java/com/cvizard/converter/services/ResumeService.java
+++ backend/converter/src/main/java/com/cvizard/converter/services/ResumeService.java
@@ -1,5 +1,7 @@
 package com.cvizard.converter.services;
 
+import com.cvizard.converter.ChatGPTRequest;
+import com.cvizard.converter.ChatGPTResponse;
 import com.cvizard.converter.interfaces.ChatGPTClient;
 import com.cvizard.converter.models.Resume;
 import com.cvizard.converter.repositories.ResumeRepository;
@@ -8,8 +10,10 @@ import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.Charset;
 import java.util.UUID;
 
 @Service
@@ -17,30 +21,28 @@ import java.util.UUID;
 public class ResumeService {
     private final ResumeRepository resumeRepository;
     private final ChatGPTClient chatGPTClient;
-    private final Gson gson;
-    @Value("${settings.prompt}")
-    private String prompt;
+//    @Value("${classpath:prompt.txt}")
+//    private Resource prompt;
+    private final String prompt = "you have here plain text from cv: [RESUME TEXT], analyse this and return me only this JSON template: { \"work\": { [ \"name\": \"\", \"position\": \"\", \"startDate\": \"\", \"endDate\": \"\", \"summary\": \"\", ] }, \"education\": { [ \"institution\": \"\", \"area\": \"\", \"studyType\": \"\", \"startDate\": \"\", \"endDate\": \"\" ] }, \"certificates\": { [ \"name\": \"\", \"date\": \"\", \"issuer\": \"\" ] }, \"skills\": { [ \"name\": \"\", \"level\": \"\", \"keywords\": [] ] }, \"languages\": { [ \"language\": \"\", \"level\": \"\" ] }, \"projects\": { [ \"name\": \"\", \"startDate\": \"\", \"endDate\": \"\", \"summary\": \"\" ] }, \"interests\": { [ \"name\": \"\", \"keywords\": [] ] } } with approximately parsed data from the given text";
     @Value("${settings.gpt-api-key}")
     private String apiKey;
     @Value("${settings.model}")
     private String model;
+
+    @SneakyThrows
      public Resume resumeConverter(String resumeText, String key){
          String promptWithText = prompt.replace("[RESUME TEXT]",resumeText);
-         String tempText = "{   \"Education\": [     {       \"institution\": \"Technikum Ekonomiczne nr 1 im. Mikołaja Kopernika\",       \"area\": \"Technik ekonomista\",       \"studyType\": \"średnie\",       \"startDate\": \"09.2019\",       \"endDate\": \"05.2023\"     }   ],   \"Certificate\": [],   \"Skill\": [     {       \"name\": \"Poczucie humoru\",       \"level\": \"\",       \"keywords\": []     },     {       \"name\": \"Znajdowanie nieoczywistych rozwiązań\",       \"level\": \"\",       \"keywords\": []     },     {       \"name\": \"Umiejętność dogadania się z każdym\",       \"level\": \"\",       \"keywords\": []     }   ],   \"Language\": [     {       \"language\": \"angielski\",       \"level\": \"poziom zaawansowany\"     }   ],   \"Project\": [],   \"Interest\": [     {       \"name\": \"Czytanie książek samorozwojowych\",       \"keywords\": []     },     {       \"name\": \"aktywności fizyczne\",       \"keywords\": [         \"pływanie\",         \"bieganie\",         \"koszykówka\"       ]     },     {       \"name\": \"analyse this\",       \"keywords\": []     }   ] }";
-//         ChatGPTRequest request = new ChatGPTRequest(model,promptWithText);
-//         ChatGPTResponse response = chatGPTClient.generateChatGPTResponse("Bearer "+apiKey,request);
-//         System.out.println(response.getChoices().get(0).getMessage());
-         Resume resume = resumeParser(tempText);
+         ChatGPTRequest request = new ChatGPTRequest(model,promptWithText);
+         ChatGPTResponse response = chatGPTClient.generateChatGPTResponse("Bearer "+apiKey,request);
+        System.out.println(response.getChoices().get(0).getMessage().getContent());
+         Resume resume = resumeParser(response.getChoices().get(0).getMessage().getContent());
          resume.setId(key);
-         System.out.println(resume);
          resumeRepository.save(resume);
-         System.out.println("zapisane");
          return resume;
      }
      @SneakyThrows
      public Resume resumeParser(String resumeText){
          ObjectMapper objectMapper = new ObjectMapper();
-//         objectMapper.registerModule(new JavaTimeModule());
          return objectMapper.readValue(resumeText, Resume.class);
      }
 }

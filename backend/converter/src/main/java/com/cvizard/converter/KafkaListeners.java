e4a26f1efa580ade0a3e9b5d5ebf810b3cb89e7a
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-18 09:01:15 +0200

    fix kafka listener

--- backend/converter/src/main/java/com/cvizard/converter/KafkaListeners.java
+++ backend/converter/src/main/java/com/cvizard/converter/KafkaListeners.java
@@ -3,6 +3,7 @@ package com.cvizard.converter;
 import com.cvizard.converter.services.ResumeService;
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ import java.util.UUID;
 public class KafkaListeners {
     private final ResumeService resumeService;
     @KafkaListener(topics = "${settings.kafka.topics.cleaned-text}", groupId = "ooo")
-    void listener(@Payload String data, @Header String key){
+    void listener(@Payload String data, @Header(KafkaHeaders.RECEIVED_KEY) String key){
         resumeService.resumeConverter(data, key);
     }
 }

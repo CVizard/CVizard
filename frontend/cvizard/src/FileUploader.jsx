9daafa11b610e036eeed005f19a02977b3888256
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-19 14:53:16 +0200

    add status for uploading file

--- frontend/cvizard/src/FileUploader.jsx
+++ frontend/cvizard/src/FileUploader.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 export function FileUploader() {
   const [file, setFile] = useState(null);
   const [responseData, setResponseData] = useState(null);
+  const [feedback, setFeedback] = useState("");
 
   const onFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -11,6 +12,7 @@ export function FileUploader() {
 
   const onFileUpload = () => {
     const formData = new FormData();
+    setFeedback("Uploading ...");
     formData.append("pdf_file", file);
     axios
       .post("http://localhost:8080/api/reader", formData)
@@ -18,15 +20,17 @@ export function FileUploader() {
         console.log(response);
         setResponseData(response.data.file_id);
         localStorage.setItem("file_id", response.data.file_id);
+        setFeedback("Uploaded!");
       })
       .catch((error) => {
         console.error("There was an error!", error);
+        setFeedback("Error!");
       });
     formData.append("pdf_file", file);
   };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <div className="text-white border-solid border-8 w-max flex items-center bg-blue-500 px-6 py-6 border-blue-400 font-thin">
         <input type="file" onChange={onFileChange} accept="application/pdf" />
         <button
@@ -35,6 +39,16 @@ export function FileUploader() {
         >
           Upload!
         </button>
+      </div>
+      <div className="h-32">
+        <p>
+          {feedback && (
+            <p>
+              Status: <br />
+              {feedback}
+            </p>
+          )}
+        </p>
       </div>
     </div>
   );

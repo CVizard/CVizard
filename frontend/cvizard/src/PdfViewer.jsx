1b6d98c41b5f96ed2380e0e61307729510e0a08c
Paweł Solecki <pawelll.solecki@gmail.com>
2023-07-19 11:04:47 +0200

    frontend download output pdf

--- frontend/cvizard/src/PdfViewer.jsx
+++ frontend/cvizard/src/PdfViewer.jsx
@@ -1,23 +1,43 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
+import FileSaver from "file-saver";
 import axios from "axios";
 
 export const PdfViewer = () => {
-  const [fileId, setFileId] = useState('');
+  const [fileId, setFileId] = useState("");
   const [file, setFile] = useState(null);
 
   const handleButtonClick = () => {
     // Assuming you're using a library like axios for HTTP requests
     // Make the GET request to fetch the PDF file
     axios
-      .get(`url?key=${fileId}`)
-      .then(response => {
+      .get(`http://localhost:8080/api/creator?key=${fileId}`)
+      .then((response) => {
         // Handle the response, e.g., display the PDF
-        setFile(URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' })));
+        setFile(
+          URL.createObjectURL(
+            new Blob([response.data], { type: "application/pdf" })
+          )
+        );
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle any errors
-        console.error('Error fetching PDF:', error);
+        console.error("Error fetching PDF:", error);
+      });
+  };
+  const downloadPdf = () => {
+    axios
+      .get("http://localhost:8080/api/creator", {
+        params: {
+          key: fileId,
+        },
+        responseType: "blob", // Important
+      })
+      .then((response) => {
+        FileSaver.saveAs(new Blob([response.data]), "output.pdf");
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
 
@@ -26,14 +46,14 @@ export const PdfViewer = () => {
       <input
         type="text"
         value={fileId}
-        onChange={event => setFileId(event.target.value)}
+        onChange={(event) => setFileId(event.target.value)}
       />
-      <button onClick={handleButtonClick}>Fetch PDF</button>
+      <button onClick={downloadPdf}>Fetch PDF</button>
       <div>
-      <Document file={file}>
-        <Page pageNumber={1} />
-      </Document>
-    </div>
+        <Document file={file}>
+          <Page pageNumber={1} />
+        </Document>
+      </div>
     </div>
   );
 };
